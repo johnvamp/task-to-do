8f6e598c135a89a2bf90f53cd4739f3317821133
John <john_vamp@hotmail.com>
2022-08-09 16:40:03 -0500

    Se configura los paquetes que necesitamos yargs y colors, u se inicia el archivo app

---
+++ app.js
@@ -0,0 +1,17 @@
+const argv = require("yargs").argv;
+
+let commmand = argv._[0];
+
+switch (commmand) {
+	case "crear":
+		console.log("Crea una tarea");
+		break;
+	case "listar":
+		console.log("Lista todas las tareas");
+		break;
+	case "actualizar":
+		console.log("Actualiza una tarea");
+		break;
+	default:
+		console.log("comando no reconocido");
+}
